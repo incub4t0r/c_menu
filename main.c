c17021ca3597acee62e54b3c192eaa8121cfc711
Daniel Chung <daniel.y.chung75@account.cybbh.space>
2023-10-20 13:45:03 +0000

    updated menulib to alloc options

--- main.c
+++ main.c
@@ -27,5 +27,16 @@ main(void)
     menu_add_option(main_menu, action1, "Act 1");
     menu_add_option(main_menu, action2, "Act 2");
     menu_add_option(main_menu, action3, "Act 3");
+    menu_add_option(main_menu, action3, "Act 4");
+    menu_add_option(main_menu, action3, "Act 5");
+    menu_add_option(main_menu, action3, "Act 6");
+    menu_add_option(main_menu, action3, "Act 7");
+    menu_add_option(main_menu, action3, "Act 8");
+    menu_add_option(main_menu, action3, "Act 9");
+    menu_add_option(main_menu, action3, "Act 10");
+    menu_add_option(main_menu, action3, "Act 11");
+    menu_add_option(main_menu, action3, "Act 12");
+    menu_add_option(main_menu, action3, "Act 13");
+    menu_add_option(main_menu, action3, "Act 14");
     menu_display(main_menu);
 }
--- menulib/menulib.c
+++ menulib/menulib.c
@@ -21,46 +21,11 @@ typedef struct menu_item_t
 
 typedef struct menu_t
 {
-    char        *title;
-    int          num_options;
-    menu_item_t *options[10];
+    char         *title;
+    int           size;
+    int           capacity;
+    menu_item_t **options;
 } menu_t;
-
-/**
- * @brief Adds an option to a menu
- *
- * @param menu A pointer representing the menu
- * @param action A pointer to a function to run when the option is chosen
- * @param opt_name A char array containing the option name for the menu
- * @return int If successful, returns 1, else 0
- */
-int
-menu_add_option(menu_t *menu, void (*action)(), const char *opt_name)
-{
-    menu_item_t *item = malloc(sizeof(menu_item_t));
-
-    if (!item)
-    {
-        return 0;
-    }
-
-    if (menu->num_options
-        < (int)(sizeof(menu->options) / sizeof(menu->options[0])))
-    {
-        item->action                     = action;
-        item->option_id                  = menu->num_options;
-        item->option_name                = strdup(opt_name);
-        menu->options[menu->num_options] = item;
-        menu->num_options++;
-        return 0;
-    }
-
-    else
-    {
-        printf("menu_t full. No more options can be added.\n");
-        return 0;
-    }
-}
 
 /**
  * @brief Create a menu
@@ -79,9 +44,64 @@ menu_create(const char *menu_name)
         return NULL;
     }
 
-    new_menu->num_options = 0;
-    new_menu->title       = strdup(menu_name);
+    new_menu->size     = 0;
+    new_menu->capacity = 2;
+    new_menu->title    = strdup(menu_name);
+    new_menu->options  = malloc(new_menu->capacity * sizeof(new_menu->options));
+
+    if (!new_menu->options)
+    {
+        free(new_menu);
+        return(NULL);
+    }
+
     return new_menu;
+}
+
+/**
+ * @brief Adds an option to a menu
+ *
+ * @param menu A pointer representing the menu
+ * @param action A pointer to a function to run when the option is chosen
+ * @param opt_name A char array containing the option name for the menu
+ * @return int If successful, returns 1, else 0
+ */
+int
+menu_add_option(menu_t *menu, void (*action)(), const char *opt_name)
+{
+    menu_item_t *item = malloc(sizeof(menu_item_t));
+
+    if (!item)
+    {
+        return 0;
+    }
+    item->action              = action;
+    item->option_id           = menu->size;
+    item->option_name         = strdup(opt_name);
+    printf("Alloc'd and set new option\n");
+
+    if (menu->size == menu->capacity)
+    {
+        menu_item_t **tmp = realloc(menu->options, 2 * menu->capacity * sizeof(menu->options));
+
+        if (!tmp)
+        {
+            return 0;
+        }
+
+        menu->options = tmp;
+        menu->capacity *= 2;
+    }
+    printf("Adding new option\n");
+    menu->options[menu->size] = item;
+    menu->size++;
+    return 1;
+
+    // else
+    // {
+    //     printf("menu_t full. No more options can be added.\n");
+    //     return 0;
+    // }
 }
 
 /**
@@ -92,12 +112,12 @@ menu_create(const char *menu_name)
 void
 menu_free(menu_t *menu)
 {
-    for (int idx = 0; idx < menu->num_options; idx++)
+    for (int idx = 0; idx < menu->size; idx++)
     {
         free(menu->options[idx]->option_name);
         free(menu->options[idx]);
     }
-
+    free(menu->options);
     free(menu->title);
     free(menu);
 }
@@ -117,7 +137,7 @@ menu_display(menu_t *menu)
     {
         printf("\n\nmenu_t: %s\n", menu->title);
 
-        for (int idx = 0; idx < menu->num_options; idx++)
+        for (int idx = 0; idx < menu->size; idx++)
         {
             printf("%d: %s\n",
                    menu->options[idx]->option_id + 1,
@@ -135,7 +155,7 @@ menu_display(menu_t *menu)
 
         int found = 0;
 
-        for (int idx = 0; idx < menu->num_options; idx++)
+        for (int idx = 0; idx < menu->size; idx++)
         {
             if (menu->options[idx]->option_id == choice - 1)
             {
